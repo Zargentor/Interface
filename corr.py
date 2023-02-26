d110ebba486964f3c8bf028a56a12feac31c7a3d
Zargentor <Zarger89@gmail.com>
2023-02-26 17:10:31 +0400

    Нахуевертил и спит x3

--- corr.py
+++ corr.py
@@ -10,14 +10,13 @@ root['bg'] = '#fafafa'
 # Указываем название окна
 root.title('Поиск по слову')
 # Указываем размеры окна
-root.geometry('800x600')
+root.geometry('1000x600')
 # Делаем невозможным менять размеры окна
 root.resizable(width=False, height=False)
 
 couttext = ""
 global dirpath
 def ButtonOne():
-
     dirpath = filedialog.askdirectory(
         parent=textworkgroup,
         title='Выберите директорию поиска'
@@ -31,9 +30,9 @@ def ButtonOne():
     textpool = Entry(textworkgroup)
     textpool.place(width=500, height=50, y=45)
 
-    btn_two = Button(buttonworkgroup, text="Начать поиск", command=ButtonTwo)
+    btn_two = Button(buttonworkgroup, text="Начать поиск", command=lambda:ButtonTwo(dirpath))
     btn_two.place(width=100, height=25, y=50)
-    ButtonTwo(dirpath)
+
 
 def ButtonTwo(dirpath):
     phraseforword = Label(textworkgroup, text="Введите слово для поиска", anchor='w', )
@@ -44,11 +43,12 @@ def ButtonTwo(dirpath):
     s = textpool.get()
     print(s)
     if s != None:
-        interface(s,dirpath)
-    cout = Text(textworkgroup,)
+        interface(s, dirpath)
+    cout = Text(textworkgroup)
+    cout.insert(1.0,perebor_corr())
     cout.place(height=500, width=500, y=95)
     scrollright = Scrollbar(cout, command=cout.yview)
-    scrollright.pack(side=LEFT, fill=Y)
+    scrollright.pack(side=RIGHT, fill=Y)
 
 
 
